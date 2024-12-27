93dac89efaa24b8341bc4a93e6224ba8f5e001bd
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2024-12-28 01:12:31 +0200

    commit for required 2

--- main.dart
+++ main.dart
@@ -1,7 +1,8 @@
 import 'bank_account.dart';
 
 void main() {
-  BankAccount account1 = BankAccount('1008181655', 1000);
+  BankAccount account1 = BankAccount('1008181655',
+      1000); // created account named account1 with accountID and balance.
   account1.displayAccountInfo();
   account1.withdraw(-500);
   account1.displayAccountInfo();
@@ -9,10 +10,11 @@ void main() {
   account1.displayAccountInfo();
   account1.deposit(200);
   account1.displayAccountInfo();
-  BankAccount account2 = BankAccount.id('1009191655');
+  BankAccount account2 = BankAccount.id(
+      '1009191655'); // created account named account2 with accountID and balance value defualt is 0.
   account2.displayAccountInfo();
   account2.withdraw(500);
   account2.displayAccountInfo();
-  account2.deposit(200);
+  account2.deposit(300);
   account2.displayAccountInfo();
 }
